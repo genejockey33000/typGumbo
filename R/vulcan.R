0cdf3519c7e531e44165a87be8a5ed95d0f591bc
= <=>
2021-08-17 18:25:07 -0400

    vulcan info

--- R/vulcan.R
+++ R/vulcan.R
@@ -1,12 +1,15 @@
 #' Vulcan: Generate easy volcano plots from sleuth results test tables
 #'
-#' Takes the output of sleuth_results() and generates a volcano plot with relative ease and some key adjustable parameters
+#' Takes the output of sleuth_results() and generates a volcano plot with relative ease
+#' and includes a few key adjustable parameters. Test table is trimmed of all rows with
+#' NAs (retained in sleuth_results()). ENSGs without mapped ext_gene names are also trimmed.
 #'
 #' @param x Output from sleuth_results() or any test tables with values for b and for qval
 #' @param bcut Cutoff value for b (log fold change) for significance
 #' @param qcut Cutoff value for qval (adjusted pval) for significance
 #' @param labels How many of the top DEG data points to label. Top DEGs are identified
 #'   buy calculating the rank = -lot10(qval) * bval. Vulcan will plots the top and bottom scores.
+#'   Default is 0 and prints no labels.
 #' @param repel Force of point label repulsion (from geom_text_repel()). Default = 1
 #' @param pad Padding around label boxes (from geom_text_repel()). Default = 0.25
 #' @param labelsize Size of labels (from geom_text_repel()). Default = 4
