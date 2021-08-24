f4e543dc0a4340a1d548386ca9fe57a1a6332c8d
= <=>
2021-08-24 11:40:06 -0400

    cleanXM bug fix

--- R/cleanXM.R
+++ R/cleanXM.R
@@ -1,7 +1,12 @@
 #' Expression matrix cleaning tool
 #'
 #' This is a short pipeline that filters expression matrices based on k over A
-#' arguments, log2() transforms and quantile normalizes
+#' arguments, log2() transforms and quantile normalizes. Unnecessary for ratiometric
+#' data or previously processed data but useful for raw or TPM matrices.
+#' Function will remove all measurements with 0 values
+#' in all samples (zero variance). It will test matrix for asymmetric negative values (often resulting from
+#' regressing batch effects) and raise all values by abs(smallest negative value) to remove any negative
+#' expression measurement.
 #'
 #' @param x Input expression matrix, measurements in rows
 #' @param k Number of samples whose expression need to be higher than A
@@ -12,7 +17,11 @@
 #'
 cleanXM <- function(x, k = 0, A = 0) {
   x <- x[(apply(x,1,var) > 0),] #eliminates measurements with all zeros
-  x <- x + base::abs(min(x)) #for matrices with negative values. Lifts all values by lowest value
+  if (min(x) < 0 & max(x) / (abs(min(x))) > 100) {
+    #for matrices with unintended negative values after regression.
+    #Lifts all values by abs() of lowest (negative) value
+  x <- x + base::abs(min(x))
+  }
   if(k > 0) {
     x <- filt(x, k = k, A = A)
     if(nrow(x) == 0) stop("Those filtering parameters removed all measurements.
