256e02b3fc75a9e08a4e8c6845f4aaf9e6dca807
= <=>
2022-02-04 15:42:28 -0500

    added rcorradj, flatten, and m2mCorrPlot

---
+++ R/corrdot.R
@@ -0,0 +1,21 @@
+#' Make a linear regression dot plot of two vectors in a matrix
+#'
+#' @param mat Single matrix input
+#' @param x quoted name of column data for x axis (i.e. "Abeta42_40)
+#' @param y quoted name of column data for y axis (i.e. "Tau-p")
+#' @param datatype quoted. What kind of data is this? (i.e. "RNAseq", "Proteomics", "Assay")
+#' @param title quoted. Title of plot to generate
+#' @param method Statistical method for correlation reporting ("pearson" or "spearman")
+#'
+#' @return
+#' @export
+#'
+#' @examples
+corrdot <- function(mat, x, y, datatype, title, method = "pearson") {
+  x1 <- as.numeric(mat[,x])
+  y1 <- as.numeric(mat[,y])
+  xycor <- Hmisc::rcorr(x1,y1, type = method)
+  p <- plot(x1,y1, xlab = paste0(x, " ", datatype , "; P = ",signif(xycor$P[1,2], digits = 3) ), ylab = paste0(y, " ",datatype, "; r = ", signif(xycor$r[1,2], digits = 3)), main = paste0("Correlation between ",x," and ", y))
+  return(p +
+           abline(lm(y1~x1), col = "black"))
+}
---
+++ R/flattenCorrMatrix.R
@@ -0,0 +1,22 @@
+#' Flat matrix from Hmisc::rcorr list output
+#' Takes the r value and the pvalue objects for the rcorr list object
+#'   and returns a flat matrix for all combinations. Then sorts for
+#'   best pvals
+#'
+#' @param rcorrOUT Correlation matrix output from rcorr or rcorr adjust
+#'
+#' @return
+#' @export
+#'
+#' @examples
+flattenCorrMatrix <- function(rcorrOUT) {
+  corrmat <- rcorrOUT$r
+  pmat <- rcorrOUT$P
+  ut <- upper.tri(cormat)
+  data.frame(
+    row = rownames(cormat)[row(cormat)[ut]],
+    column = rownames(cormat)[col(cormat)[ut]],
+    cor  =(cormat)[ut],
+    p = pmat[ut]
+  )
+}
