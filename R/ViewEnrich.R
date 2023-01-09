085efe0204af58e63b001d4a82931f0ecb83287e
= <=>
2023-01-09 14:00:54 -0500

    enhanced ViewEnrich to focus on CC, BP or MF and return best qvals

--- R/ViewEnrich.R
+++ R/ViewEnrich.R
@@ -2,11 +2,23 @@
 #'   simple viewer of significantly enriched pathways that are output from mkEnrich()
 #'
 #' @param x Output object from mkEnrich()
+#' @param view What to focus on for this view. One of "best.qvals" (default),
+#'   "best.CCs" (most sig. in Cellular category)
+#'   "best.BPs" (most sig. in Biological Process category)
+#'   "best.MFs" (most sig. in Molecular Function category)
 #'
 #' @return Streamlined version of results from mkEnrich object
 #' @export
 #'
-ViewEnrich <- function(x) {
+ViewEnrich <- function(x, view = "best.qvals") {
   quickView <- as.data.frame(x@result[,c(1,3,4,8,9)])
+  quickView <- quickView[order(quickView$qvalue),]
+  if (view == "best.CCs") {
+    quickView <- quickView[grepl(pattern = "CC", quickView$ONTOLOGY),]
+  } else if (view == "best.BPs") {
+    quickView <- quickView[grepl(pattern = "BP", quickView$ONTOLOGY),]
+  } else if (view == "best.MFs") {
+    quickView <- quickView[grepl(pattern = "MF", quickView$ONTOLOGY),]
+  }
   View(quickView)
 }
