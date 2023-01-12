98057160bb7e78a990572aedd6983c2c87021d25
= <=>
2023-01-12 09:10:45 -0500

    add ont argument to mkEnrich

--- R/mkEnrich.R
+++ R/mkEnrich.R
@@ -4,13 +4,14 @@
 #' Can take a bit of time to run
 #'
 #' @param x quoted path to *.csv file containing ONE column of gene names
+#' @param ont Enter one of "ALL" (default), "BP", "MF", "CC"
 #' @param qvalueCutoff Highest qvalue returned (default = .01)
 #'
 #' @return GO enrichment object
 #' @importFrom clusterProfiler enrichGO
 #' @export
-mkEnrich <- function(x, qvalueCutoff = 0.01) {
+mkEnrich <- function(x, ont = "ALL", qvalueCutoff = 0.01) {
   input <- scan(file = x, what = character(), sep = ",")
-  inputGO <- clusterProfiler::enrichGO(input, OrgDb = "org.Hs.eg.db", ont="ALL", keyType = "SYMBOL", qvalueCutoff = qvalueCutoff)
+  inputGO <- clusterProfiler::enrichGO(input, OrgDb = "org.Hs.eg.db", ont=ont, keyType = "SYMBOL", qvalueCutoff = qvalueCutoff)
   return(inputGO)
 }
