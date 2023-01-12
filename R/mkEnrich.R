ce6ff26a0d913b5733e2b417d68376c61546fea7
= <=>
2023-01-12 09:58:53 -0500

    added ReactomePA to mkEnrich() function

--- R/mkEnrich.R
+++ R/mkEnrich.R
@@ -4,14 +4,21 @@
 #' Can take a bit of time to run
 #'
 #' @param x quoted path to *.csv file containing ONE column of gene names
-#' @param ont Enter one of "ALL" (default), "BP", "MF", "CC"
+#' @param db One of "GO" (default), or "Reactome". May add more later.
+#' @param GOont Enter one of "ALL" (default), "BP", "MF", "CC"
 #' @param qvalueCutoff Highest qvalue returned (default = .01)
 #'
 #' @return GO enrichment object
 #' @importFrom clusterProfiler enrichGO
 #' @export
-mkEnrich <- function(x, ont = "ALL", qvalueCutoff = 0.01) {
+mkEnrich <- function(x, db = "GO",GOont = "ALL", qvalueCutoff = 0.01) {
   input <- scan(file = x, what = character(), sep = ",")
-  inputGO <- clusterProfiler::enrichGO(input, OrgDb = "org.Hs.eg.db", ont=ont, keyType = "SYMBOL", qvalueCutoff = qvalueCutoff)
-  return(inputGO)
+  input <- unique(input)
+  if (db == "Reactome") {
+    input <- clusterProfiler::bitr(input, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = "org.Hs.eg.db")
+    output <- ReactomePA::enrichPathway(input$ENTREZID, qvalueCutoff = qvalueCutoff, readable = TRUE)
+  } else {
+  output <- clusterProfiler::enrichGO(input, OrgDb = "org.Hs.eg.db", ont=GOont, keyType = "SYMBOL", qvalueCutoff = qvalueCutoff)
+  }
+  return(output)
 }
