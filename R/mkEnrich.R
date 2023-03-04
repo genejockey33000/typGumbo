a53c4c77a9f961e99ca0e453c1508a6883297916
= <=>
2023-03-04 13:01:53 -0500

    bug fixes mkEnrich()

--- R/mkEnrich.R
+++ R/mkEnrich.R
@@ -6,7 +6,7 @@
 #' Can take a bit of time to run
 #'
 #' @param x quoted path to *.csv file containing ONE column of gene names
-#' @param db One of "GO" (default), "Reactome", "KEGG", or "MKEGG". May add more later.
+#' @param db One of "GO" (default), "Reactome", or "MKEGG". May add more later.
 #' @param GOont Enter one of "ALL" (default), "BP", "MF", "CC"
 #' @param qvalueCutoff Highest qvalue returned (default = .01)
 #'
@@ -14,24 +14,22 @@
 #' @importFrom clusterProfiler enrichGO
 #' @export
 mkEnrich <- function(x, db = "GO",GOont = "ALL", qvalueCutoff = 0.01) {
-  if (db %in% c("Reactome", "KEGG", "MKEGG", "GO")) {
-    cat("Preparing ", db, " analysis!")
+  if (db %in% c("Reactome", "MKEGG", "GO")) {
+    cat("Preparing ", db, " analysis!\n")
   } else {stop("Srrrry! db argument must be either 'GO' (default), 'Reactome', 'KEGG', or 'MKEGG' (Module KEGG).")}
   input <- scan(file = x, what = character(), sep = ",")
   input <- unique(input)
   if (db == "Reactome") {
     input <- clusterProfiler::bitr(input, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = "org.Hs.eg.db")
     output <- ReactomePA::enrichPathway(input$ENTREZID, qvalueCutoff = qvalueCutoff, readable = TRUE)
-  } else if (db == "KEGG") {
-    input <- clusterProfiler::bitr(input, fromType = "SYMBOL", toType = c("ENTREZID"), OrgDb = "org.Hs.eg.db")
-    input <- clusterProfiler::bitr_kegg(input$ENTREZID, fromType = "ncbi-geneid", toType = "kegg", organism = "hsa")
-    output <- clusterProfiler::enrichKEGG(input$kegg, organism = "hsa", keyType = "kegg", pAdjustMethod = "BH", qvalueCutoff = qvalueCutoff)
-  } else if (db == "MKEGG") {
-    input <- clusterProfiler::bitr(input, fromType = "SYMBOL", toType = c("ENTREZID"), OrgDb = "org.Hs.eg.db")
+  }
+  if (db == "MKEGG") {
+    input <- clusterProfiler::bitr(input, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = "org.Hs.eg.db")
     input <- clusterProfiler::bitr_kegg(input$ENTREZID, fromType = "ncbi-geneid", toType = "kegg", organism = "hsa")
     output <- clusterProfiler::enrichMKEGG(input$kegg, organism = "hsa", keyType = "kegg", pAdjustMethod = "BH", qvalueCutoff = qvalueCutoff)
-  } else if (db == "GO") {
-  output <- clusterProfiler::enrichGO(input, OrgDb = "org.Hs.eg.db", ont=GOont, keyType = "SYMBOL", qvalueCutoff = qvalueCutoff)
+  }
+  if (db == "GO") {
+    output <- clusterProfiler::enrichGO(input, OrgDb = "org.Hs.eg.db", ont=GOont, keyType = "SYMBOL", qvalueCutoff = qvalueCutoff)
   }
   return(output)
 }
