6664ebd714f17711ac4ffcba2d02a22b17c75d2e
= <=>
2023-03-04 13:30:47 -0500

    bugs mkEnrich v3

--- R/mkEnrich.R
+++ R/mkEnrich.R
@@ -3,22 +3,28 @@
 #' @description Simple script that uses clusterProfiler to generate GO enrichment object.
 #' Takes single column .csv file of gene symbols (i.e. "MAPT", "NEUROG2") as input (no column header).
 #' Outputted object can be fed into downstream functions like gene concept network plots
-#' Can take a bit of time to run
+#' Can take a bit of time to run.
 #'
-#' @param x quoted path to *.csv file containing ONE column of gene names
+#' @param csv quoted path to *.csv file containing ONE column of gene names
 #' @param db One of "GO" (default), "Reactome", or "MKEGG". May add more later.
 #' @param GOont Enter one of "ALL" (default), "BP", "MF", "CC"
 #' @param qvalueCutoff Highest qvalue returned (default = .01)
 #'
 #' @return GO enrichment object
+#' @importFrom clusterProfiler bitr
+#' @importFrom clusterProfiler bitr_kegg
 #' @importFrom clusterProfiler enrichGO
+#' @importFrom clusterProfiler enrichMKEGG
+#' @importFrom ReactomePA enrichPathway
 #' @export
-mkEnrich <- function(x, db = "GO",GOont = "ALL", qvalueCutoff = 0.01) {
+mkEnrich <- function(csv, db = "GO", GOont = "ALL", qvalueCutoff = 0.01) {
   if (db %in% c("Reactome", "MKEGG", "GO")) {
     cat("Preparing ", db, " analysis!\n")
   } else {stop("Srrrry! db argument must be either 'GO' (default), 'Reactome', 'KEGG', or 'MKEGG' (Module KEGG).")}
-  input <- scan(file = x, what = character(), sep = ",")
+
+  input <- scan(file = csv, what = character(), sep = ",")
   input <- unique(input)
+
   if (db == "Reactome") {
     input <- clusterProfiler::bitr(input, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = "org.Hs.eg.db")
     output <- ReactomePA::enrichPathway(input$ENTREZID, qvalueCutoff = qvalueCutoff, readable = TRUE)
