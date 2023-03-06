ca931beb7f73b6f9e7cf9815c9319dd34352b889
= <=>
2023-03-06 08:50:07 -0500

    debugging install issues.

--- R/mkEnrich.R
+++ R/mkEnrich.R
@@ -1,5 +1,6 @@
-#' Make Gene Ontology Enrichment Object
-#' Simple script that uses clusterProfiler to generate GO enrichment object.
+#' @title Make Gene Ontology Enrichment Object
+#'
+#' @description Simple script that uses clusterProfiler to generate GO enrichment object.
 #' Takes single column .csv file of gene symbols (i.e. "MAPT", "NEUROG2") as input (no column header).
 #' Outputted object can be fed into downstream functions like gene concept network plots
 #' Can take a bit of time to run.
@@ -13,16 +14,21 @@
 #' @importFrom clusterProfiler bitr
 #' @importFrom clusterProfiler enrichGO
 #' @importFrom ReactomePA enrichPathway
-#' @importClassesFrom Matrix index
 #' @export
-mkEnrich <- function(csv, db = "GO", GOont = "ALL", qvalueCutoff = 0.01) {
-
-  input <- scan(file = csv, what = character(), sep = ",")
+mkEnrich <- function(x, db = "GO",GOont = "ALL", qvalueCutoff = 0.01) {
+  if (db %in% c("Reactome", "MKEGG", "GO")) {
+    cat("Preparing ", db, " analysis!\n")
+  } else {stop("Srrrry! db argument must be either 'GO' (default), 'Reactome', or 'MKEGG' (Module KEGG).")}
+  input <- scan(file = x, what = character(), sep = ",")
   input <- unique(input)
-
   if (db == "Reactome") {
     input <- clusterProfiler::bitr(input, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = "org.Hs.eg.db")
     output <- ReactomePA::enrichPathway(input$ENTREZID, qvalueCutoff = qvalueCutoff, readable = TRUE)
+  }
+  if (db == "MKEGG") {
+    input <- clusterProfiler::bitr(input, fromType = "SYMBOL", toType = "ENTREZID", OrgDb = "org.Hs.eg.db")
+    input <- clusterProfiler::bitr_kegg(input$ENTREZID, fromType = "ncbi-geneid", toType = "kegg", organism = "hsa")
+    output <- clusterProfiler::enrichMKEGG(input$kegg, organism = "hsa", keyType = "kegg", pAdjustMethod = "BH", qvalueCutoff = qvalueCutoff)
   }
   if (db == "GO") {
     output <- clusterProfiler::enrichGO(input, OrgDb = "org.Hs.eg.db", ont=GOont, keyType = "SYMBOL", qvalueCutoff = qvalueCutoff)
