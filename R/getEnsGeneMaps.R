807228aa637287f7cbee3fc9bcff52f9f8f99758
= <=>
2023-06-09 15:14:32 -0400

    update getGeneMaps function to work.

--- R/getEnsGeneMaps.R
+++ R/getEnsGeneMaps.R
@@ -21,36 +21,36 @@ getEnsGeneMaps <- function(type = c("full","t2g","t2n","g2n","g2e","n2e")){
   mart <- biomaRt::useMart(biomart = "ENSEMBL_MART_ENSEMBL", dataset = "hsapiens_gene_ensembl", host = 'https://www.ensembl.org')
   if (type == "full") {
     map <- biomaRt::getBM(attributes = c("ensembl_transcript_id", "ensembl_gene_id","external_gene_name", "chromosome_name"), mart = mart)
-    map <- dplyr::rename(map, target_id = map$ensembl_transcript_id, ens_gene = map$ensembl_gene_id, ext_gene = map$external_gene_name, chr = map$chromosome_name)
-    return(map)
+    colnames(map) <- c("enst", "ensg", "gene", "chr")
+        return(map)
   }
   if (type == "t2g") {
     map <- biomaRt::getBM(attributes = c("ensembl_transcript_id", "ensembl_gene_id"), mart = mart)
-    map <- dplyr::rename(map, target_id = map$ensembl_transcript_id, ens_gene = map$ensembl_gene_id)
+    colnames(map) <- c("enst", "ensg")
     map <- typGumbo::noNANA(map)
     return(map)
   }
   if (type == "t2n") {
     map <- biomaRt::getBM(attributes = c("ensembl_transcript_id", "external_gene_name"), mart = mart)
-    map <- dplyr::rename(map, target_id = map$ensembl_transcript_id, ext_gene = map$external_gene_name)
+    colnames(map) <- c("enst", "gene")
     map <- typGumbo::noNANA(map)
     return(map)
   }
   if (type == "g2n") {
     map <- biomaRt::getBM(attributes = c("ensembl_gene_id","external_gene_name"), mart = mart)
-    map <- dplyr::rename(map, ens_gene = map$ensembl_gene_id, ext_gene = map$external_gene_name)
+    colnames(map) <- c("ensg", "gene")
     map <- typGumbo::noNANA(map)
     return(map)
   }
   if (type == "g2e") {
     map <- biomaRt::getBM(attributes = c("ensembl_gene_id","entrezgene_id"), mart = mart)
-    map <- dplyr::rename(map, ens_gene = map$ensembl_gene_id, entrez = map$entrezgene_id)
+    colnames(map) <- c("ensg", "entrez")
     map <- typGumbo::noNANA(map)
     return(map)
   }
   if (type == "n2e") {
     map <- biomaRt::getBM(attributes = c("external_gene_name", "entrezgene_id"), mart = mart)
-    map <- dplyr::rename(map, ext_gene = map$external_gene_name, entrez = map$entrezgene_id)
+    colnames(map) <- c("gene", "entrez")
     map <- typGumbo::noNANA(map)
     return(map)
   }
